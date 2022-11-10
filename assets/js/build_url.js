d73c0a48d0dcd077c1a947a4523d8e10db9b63c9
勿埋我心 <57932548+Qiantigers@users.noreply.github.com>
2022-11-10 14:20:58 +0800

    Update build_url.js

--- assets/js/build_url.js
+++ assets/js/build_url.js
@@ -1,5 +1,5 @@
 function build_url(){
     var url = encodeURI(document.querySelector('#url').value);
-    url = "/api/?url=" + url;
-    document.getElementById("b_url").innerHTML=`<a href=${url} target='_blank'>长按或者右键复制我</a>`;
+    url = document.location.href + "/api/?url=" + url;
+    document.getElementById("b_url").innerHTML=`<a href=${url} target='_blank'>${url}</a>`;
 }
