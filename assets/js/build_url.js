f73d40d63adcbc57615543baa27394c031336376
勿埋我心 <57932548+Qiantigers@users.noreply.github.com>
2022-11-10 14:13:23 +0800

    Update build_url.js

--- assets/js/build_url.js
+++ assets/js/build_url.js
@@ -1,5 +1,5 @@
 function build_url(){
     var url = encodeURI(document.querySelector('#url').value);
-    url = "https://qiantigers.github.io/qq_fanghong/api/?url=" + url;
-    document.getElementById("b_url").innerHTML=`<a href=${url} target='_blank'>${url}</a>`;
+    url = "/api/?url=" + url;
+    document.getElementById("b_url").innerHTML=`<a href=${url} target='_blank'>长按或者右键复制我</a>`;
 }
