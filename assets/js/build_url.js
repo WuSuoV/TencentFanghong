fbf63970d5574e2dc67a88a29f15ed7d94b3055f
勿埋我心 <57932548+Qiantigers@users.noreply.github.com>
2022-11-11 16:37:22 +0800

    Update build_url.js

--- assets/js/build_url.js
+++ assets/js/build_url.js
@@ -1,5 +1,5 @@
 function build_url(){
-    var url = encodeURI(document.querySelector('#url').value);
+    var url = encodeURI(decodeURI(document.querySelector('#url').value));
     if (url === "" || url.indexOf("http") === -1){
         document.getElementById("b_url").innerHTML=`请输入链接！`;
     }
