0b3de2a62b352ed36ffd0e8d2afa69765865a85b
勿埋我心 <57932548+Qiantigers@users.noreply.github.com>
2022-11-11 17:00:36 +0800

    Update build_url.js

--- assets/js/build_url.js
+++ assets/js/build_url.js
@@ -1,7 +1,7 @@
 function build_url(){
     var url = encodeURI(decodeURI(document.querySelector('#url').value));
     if (url === "" || url.indexOf("http") === -1){
-        document.getElementById("b_url").innerHTML=`请输入链接！`;
+        document.getElementById("b_url").innerHTML=`输入的不是链接或者未加http请求头！`;
     }
     else {
         url = document.location.href + "api/?url=" + url;
